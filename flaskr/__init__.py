62183e2514f18b7dba961fdba69f09c41e950dfb
amir <amirmohammadi00184@gmail.com>
2023-03-19 00:17:44 -0400

    second

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,6 +16,9 @@ def create_app(test_config=None):
     def hello():
         return "hello world"
     
+    from .. import db
+    db.init_app(app)
+
     return app
 
 create_app().run(host='localhost')
