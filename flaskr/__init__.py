6a220a414d3976260ae3544fb954b1b0eea668a7
amir <amirmohammadi00184@gmail.com>
2023-03-20 05:56:21 -0400

    add auth and modified db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,10 +5,19 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_mapping(
-        # بعدا کاملش میکنم
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
     try:
-        os.mkdir(app.instance_path)
+        os.makedirs(app.instance_path)
     except OSError:
         pass
 
@@ -19,6 +28,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.db)
+
     return app
 
-create_app().run(host='localhost')
